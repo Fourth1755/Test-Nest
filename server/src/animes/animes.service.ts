0491e3ddb3bcb899c75d2647ead9509320970740
fourth <fourthe.hero@gmail.com>
2023-02-09 10:43:25 +0700

    Update animes.service.ts

--- server/src/animes/animes.service.ts
+++ server/src/animes/animes.service.ts
@@ -28,16 +28,16 @@ export class AnimesService {
     return this.animeRepository.find();
   }
 
-  async findOne(id: number) {
-    const animeExist = await this.animeRepository.findOne({
-      where:{
-        id:id
-      },
-    });
-    if(animeExist){
-      return animeExist
-    }else{
-      return `Can't find a #${id} anime`;
+  async findOne(id: number): Promise<Anime>{
+    try {
+      const animeExist=await this.animeRepository.findOneOrFail({
+        where:{
+          id:id
+        },
+      });
+      return animeExist;
+    } catch (error) {
+      throw error
     }
   }
 
@@ -52,15 +52,15 @@ export class AnimesService {
   }
 
   async remove(id: number) {
-    const animeExist = await this.animeRepository.findOne({
-      where:{
-        id:id
-      },
-    });
-    if(animeExist){
+    try {
+      const animeExist = await this.animeRepository.findOne({
+        where:{
+          id:id
+        },
+      });
       return await this.animeRepository.delete(id)
-    }else{
-      return `Can't find a #${id} anime`;
+    } catch (error) {
+      throw error
     }
   }
 }
